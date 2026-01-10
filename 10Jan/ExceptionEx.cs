bcb88841be6983a27b14ca49000a756af2e9f1ff
Anuska Palit <anuskapalit2004@gmail.com>
2026-01-10 18:15:51 +0530

    Delegate and Multicast

---
+++ 10Jan/ExceptionEx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{
+    public class CustomException
+    {
+        public static void Main()
+        {
+            try
+            {
+                int result = Divide(10, 0);
+                Console.WriteLine("Result: " + result);
+            }
+            catch (AppCustomException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        private static int Divide(int v1, int v2)
+        {
+            try
+            {
+                return v1 / v2;
+            }
+            catch 
+            {
+
+                throw new AppCustomException();
+            }
+           
+        }
+    }
+}
---
+++ 10Jan/Exceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LearningCSharp
+{
+    public class AppCustomException : Exception
+    {
+        public override string Message => HandleBase(base.Message);
+        private string HandleBase(string sysMessage)
+        {
+            Console.WriteLine(sysMessage);
+            return "Internal Exception Ocurred. Please contract Admin";
+        }
+    }
+}
