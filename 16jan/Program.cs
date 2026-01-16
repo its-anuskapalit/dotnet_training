526f4c430fe1196d76b8851a3e0c2c437e5aca48
Anuska Palit <anuskapalit2004@gmail.com>
2026-01-16 20:07:48 +0530

    NEW questions

---
+++ 16jan/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program{
+    public static void Main()
+    {
+        int[] scores=[97,92,81,60];
+        var scoreQ=
+        from score in scores
+        where score >80
+        select score;
+
+        foreach (var i in scoreQ)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
