24bce4b44ccd71310f565fa4191c55b51973994a
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2026-02-02 15:22:09 +0530

    Update UnitTest1.cs

--- 26dec/NUnit/BankAccountApp.Tests/UnitTest1.cs
+++ 26dec/NUnit/BankAccountApp.Tests/UnitTest1.cs
@@ -1,15 +1,46 @@
-﻿namespace BankAccountApp.Tests;
+using NUnit.Framework;
+using BankAccountApp;
+using System;
 
-public class Tests
+namespace BankAccountApp.Tests
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class UnitTest
     {
-    }
+        [Test]
+        public void Test_Deposit_ValidAmount()
+        {
+            Program acc = new Program(1000);
+            acc.Deposit(500);
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            Assert.That(acc.Balance, Is.EqualTo(1500));
+        }
+
+        [Test]
+        public void Test_Deposit_NegativeAmount()
+        {
+            Program acc = new Program(1000);
+
+            var ex = Assert.Throws<Exception>(() => acc.Deposit(-100));
+            Assert.That(ex.Message, Is.EqualTo("Deposit amount cannot be negative"));
+        }
+
+        [Test]
+        public void Test_Withdraw_ValidAmount()
+        {
+            Program acc = new Program(1000);
+            acc.Withdraw(400);
+
+            Assert.That(acc.Balance, Is.EqualTo(600));
+        }
+
+        [Test]
+        public void Test_Withdraw_InsufficientFunds()
+        {
+            Program acc = new Program(500);
+
+            var ex = Assert.Throws<Exception>(() => acc.Withdraw(800));
+            Assert.That(ex.Message, Is.EqualTo("Insufficient funds."));
+        }
     }
 }
