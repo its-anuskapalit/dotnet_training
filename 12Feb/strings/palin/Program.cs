111ded7e6b0078caac0f0b586b0ecff4fee113c9
Anuska Palit <anuskapalit2004@gmail.com>
2026-02-12 10:49:49 +0530

    1/12/2026

---
+++ 12Feb/strings/palin/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static bool Palim(string input)
+    {
+        bool result=false;
+        int i=0;
+        int j= input.Length-1;
+        while (i < j)
+        {
+            if (input[i] == input[j])
+            {
+                result=true;
+            }
+            else
+            {
+                result = false;
+            }
+            i++;
+            j--;
+        }
+        return result;
+    }
+    static void Main()
+    {
+        string input=Console.ReadLine();
+        var result =Palim(input);
+        Console.WriteLine(result);
+    }
+}
