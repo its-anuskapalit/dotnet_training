aa9a8711b761935dfd4fa5ef50f94ca3d5a38f41
Anuska Palit <anuskapalit2004@gmail.com>
2026-02-12 10:33:59 +0530

    1/12/2026

---
+++ 12Feb/strings/duplicates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the size");
+        int size = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the elements");
+        List<int> input = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            input.Add(a);
+        }
+        HashSet<int> output = new HashSet<int>(input);
+        Console.WriteLine(string.Join(" , ", output));
+    }
+}
---
+++ 12Feb/strings/frequency/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+         Console.WriteLine("Enter size");
+        int size= int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter elements");
+        int[] arr=new int[5];
+        for(int i = 0; i < size; i++)
+        {
+            arr[i]=int.Parse(Console.ReadLine());
+        }
+        Dictionary<int,int> output=new Dictionary<int, int>();
+        foreach(var i in arr)
+        {
+            if (output.ContainsKey(i))
+            {
+                output[i]++;
+            }
+            else
+            {
+                output[i]=1;
+            }
+        }
+        foreach(var i in output)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
